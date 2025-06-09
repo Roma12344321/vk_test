cbdcf884994efd66f95520c1223a847ae3402409
roman <martynov.roman@wb.ru>
2025-06-10 01:20:33 +0300

    instance

--- app/VkApp.cpp
+++ app/VkApp.cpp
@@ -1,17 +1,34 @@
 #include "VkApp.h"
 
+#include <cstdlib>
+#include <cstring>
 #include <iostream>
+#include <stdexcept>
 
-const int VkApp::initialize() {
+#define WIDTH 800
+#define HEIGHT 600
+
+const std::vector<const char *> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"};
+
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
+
+int VkApp::initialize() {
   if (!glfwInit())
     return -1;
 
   glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-  window = glfwCreateWindow(640, 480, "Vulkan Demo", nullptr, nullptr);
+  window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan Demo", nullptr, nullptr);
   if (!window) {
     glfwTerminate();
     return -1;
   }
+
+  initVulkan();
 
   return 0;
 }
@@ -22,6 +39,106 @@ void VkApp::run() {
 }
 
 void VkApp::terminate() {
+  vkDestroyInstance(instance, nullptr);
   glfwDestroyWindow(window);
   glfwTerminate();
+}
+
+void VkApp::initVulkan() { createInstance(); }
+
+void VkApp::createInstance() {
+  if (enableValidationLayers && !checkValidationLayerSupport())
+    throw std::runtime_error("validation layers requested, but not available!");
+
+  VkApplicationInfo appInfo{};
+
+  appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+  appInfo.pApplicationName = "Hello Triangle";
+  appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+  appInfo.pEngineName = "No Engine";
+  appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+  appInfo.apiVersion = VK_API_VERSION_1_0;
+
+  VkInstanceCreateInfo createInfo{};
+  createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+  createInfo.pApplicationInfo = &appInfo;
+
+  auto extensions = getRequiredExtensions();
+  createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+  createInfo.ppEnabledExtensionNames = extensions.data();
+
+#if defined(__APPLE__) && defined(__MACH__)
+  std::vector<const char *> requiredExtensions;
+  for (uint32_t i = 0; i < extensions.size(); i++)
+    requiredExtensions.emplace_back(extensions[i]);
+  requiredExtensions.emplace_back(
+      VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+  createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+  createInfo.enabledExtensionCount = (uint32_t)requiredExtensions.size();
+  createInfo.ppEnabledExtensionNames = requiredExtensions.data();
+#endif
+
+  if (enableValidationLayers) {
+    createInfo.enabledLayerCount =
+        static_cast<uint32_t>(validationLayers.size());
+    createInfo.ppEnabledLayerNames = validationLayers.data();
+  } else {
+    createInfo.enabledLayerCount = 0;
+  }
+
+  if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+    throw std::runtime_error("failed to create vulkan instance!");
+}
+
+std::vector<const char *> VkApp::getRequiredExtensions() {
+  uint32_t glfwExtensionCount = 0;
+  const char **glfwExtensions;
+  glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+  std::vector<const char *> extensions(glfwExtensions,
+                                       glfwExtensions + glfwExtensionCount);
+
+  if (enableValidationLayers)
+    extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+
+  return extensions;
+}
+
+bool VkApp::checkValidationLayerSupport() {
+  uint32_t layerCount;
+  vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+  std::vector<VkLayerProperties> availableLayers(layerCount);
+  vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+  for (const char *layerName : validationLayers) {
+    bool layerFound = false;
+
+    for (const auto &layerProperties : availableLayers) {
+      if (strcmp(layerName, layerProperties.layerName) == 0) {
+        layerFound = true;
+        break;
+      }
+    }
+
+    if (!layerFound)
+      return false;
+  }
+
+  return true;
+}
+
+void VkApp::setupDebugMessenger() {
+  if (!enableValidationLayers) return;
+  
+}
+
+static VKAPI_ATTR VkBool32 VKAPI_CALL
+debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+              VkDebugUtilsMessageTypeFlagsEXT messageType,
+              const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData,
+              void *pUserData) {
+  std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+
+  return VK_FALSE;
 }
--- app/VkApp.h
+++ app/VkApp.h
@@ -1,17 +1,27 @@
 #ifndef VKAPP_H
 #define VKAPP_H
+#define GLFW_INCLUDE_VULKAN
 
 #include "../GLFW/glfw3.h"
+#include <vector>
 
 class VkApp {
 
 public:
-  const int initialize();
+  int initialize();
   void run();
   void terminate();
 
 private:
   GLFWwindow *window;
+  VkInstance instance;
+  VkDebugUtilsMessengerEXT debugMessenger;
+  
+std::vector<const char*> getRequiredExtensions();
+  void initVulkan();
+  void createInstance();
+  bool checkValidationLayerSupport();
+  void setupDebugMessenger();
 };
 
 #endif
